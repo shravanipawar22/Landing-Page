b1344bdb4c3357f1e8ea7fe17400ba44b9f4aacd
shravani pawar <shravanipawar901@gmail.com>
2025-07-07 00:51:04 +0530

    inital

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Landing-Page',
+  description: 'Landing page created with v0',
   generator: 'v0.dev',
 }
 
